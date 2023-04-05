d7ca3da6f0f0d8c504b4349ee209bb28444bbd72
fabian parentelli <fabianparentelli007@hotmail.com>
2023-04-04 21:32:05 -0300

    4abril

--- javaScript/puntosDeVenta.js/pVenta.js
+++ javaScript/puntosDeVenta.js/pVenta.js
@@ -5,16 +5,16 @@ const contPVentas = document.querySelector("#contPVentas");
 
 export function clientesDefault() {
     
-    const sanjustoDefaul = puntosDeVenta.find(pVenta => pVenta.zona === "San Justo");
-    const moronDefaul = puntosDeVenta.find(pVenta => pVenta.zona === "Moron");
-    const mataderosDefaul = puntosDeVenta.find(pVenta => pVenta.zona === "Mataderos");
-    const ramosDefaul = puntosDeVenta.find(pVenta => pVenta.zona === "Ramos Mejia");
+    const sanjustoDefaul = puntosDeVenta.find(pVenta => pVenta.sucursal === "Granja Magnas");
+    const moronDefaul = puntosDeVenta.find(pVenta => pVenta.sucursal === "Pescadería Mar de Plata");
+    const mataderosDefaul = puntosDeVenta.find(pVenta => pVenta.sucursal === "Dietética del Valle");
+    const ramosDefaul = puntosDeVenta.find(pVenta => pVenta.sucursal === "Dietética Rojas");
 
     const arrayDefaul = [sanjustoDefaul, moronDefaul, mataderosDefaul, ramosDefaul];
 
-    arrayDefaul.forEach((pVenta) => {
+    arrayDefaul.forEach((pVentaDef) => {
 
-        const {id, sucursal, direccion, mapa, zona} = pVenta;
+        const {id, sucursal, direccion, mapa, zona} = pVentaDef;
 
         const div = document.createElement("div");
         div.classList.add("cardPVenta");
